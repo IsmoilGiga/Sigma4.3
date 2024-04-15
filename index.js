966a6336edb295c9c5f5037b9b4ab9199a89c1fd
IsmoilGiga <ismoilagzamovman@gmail.com>
2024-04-15 11:39:09 +0500

    Sigma4.3

---
+++ index.js
@@ -0,0 +1,8 @@
+let taxminiySon = Math.random();
+
+console.log(taxminiySon);
+console.log(taxminiySon * 2);
+console.log(taxminiySon / 2);
+console.log(taxminiySon % 2);
+console.log(taxminiySon - 2);
+console.log(taxminiySon + 2);
